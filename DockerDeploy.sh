baaf6997ab551eb134d1dff4498e6a66eabd2d15
yan <906596588@qq.com>
2022-12-30 13:56:07 +0800

    add

--- DockerDeploy.sh
+++ DockerDeploy.sh
@@ -2,6 +2,6 @@ cd EnvDocker
 docker image build -t flask_sql_env .
 cd ..
 mv DepDocker/Dockerfile Dockerfile
-docker image build -t p105_db_op_ms .
-docker save -o p105_db_op_ms.tar p105_db_op_ms:latest
+docker image build -t zeng_db_op_ms .
+docker save -o zeng_db_op_ms.tar zeng_db_op_ms:latest
 mv Dockerfile DepDocker/Dockerfile
